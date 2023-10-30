133feb29ee9890dd7660b80ce37438b424df63a6
Vernon van Dam <vernon.vandam@gmail.com>
2023-10-30 09:23:51 +0800

    basic structure with clerk authentication

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs"
+
+export default function Home() {
+  return (
+    <div>
+    <p className='text-3xl font-medium text-sky-700'>
+      protected page
+    </p>
+    <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}

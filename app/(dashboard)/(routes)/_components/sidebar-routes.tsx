78642066efe5361f2419ca91e2a73548f39fb760
Vernon van Dam <vernon.vandam@gmail.com>
2023-10-30 22:18:45 +0800

    course layout without chapters

--- app/(dashboard)/(routes)/_components/sidebar-routes.tsx
+++ app/(dashboard)/(routes)/_components/sidebar-routes.tsx
@@ -1,34 +1,52 @@
 "use client";
 
-import { Layout, Compass } from "lucide-react";
+import { Layout, Compass, List, BarChart } from "lucide-react";
 import { SidebarItem } from "./sidebar-item";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
-    {
-        icon: Layout,
-        label: "Dashboard",
-        href: "/",
-    },
-    {
-        icon: Compass,
-        label: "Browse",
-        href: "/search",
-    },
-]
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
 
 export const SidebarRoutes = () => {
-    const routes = guestRoutes;
+  const pathname = usePathname();
 
-    return (
-        <div className="flex flex-col w-full">
-            {routes.map((route) => (
-                <SidebarItem
-                key={route.href}
-                icon={route.icon}
-                label={route.label}
-                href={route.href}                
-                />
-            ))}
-        </div>
-    )
-}
+  const isTeacherPage = pathname?.includes("/teacher");
+
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
